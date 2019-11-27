92af34cf8dc58f62e01c6e4791231858fd270e15
Nathan Chua <nchua235@mtroyal.ca>
2019-11-26 17:10:04 -0700

    test 2

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 var app = express();
 
-var server = app.listen(process.env.PORT || 8080, function () {
-    console.log("App now running on address: ", server.address());
-});
-
 app.get('/', function (req, res) {
     // res.sendFile();
     console.log("hello")
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    console.log("App now running on address: ", server.address());
 });
