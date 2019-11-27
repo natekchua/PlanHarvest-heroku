ccbbe7ba61ebd259b3449cc24436942b748a2f5a
Nathan Chua <nchua235@mtroyal.ca>
2019-11-26 17:26:03 -0700

    test 3

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+require('./connectDB').initDB();
+
+var pool = require('./connectDB').getPool();
+
 const express = require('express');
 
 var app = express();
 
 app.get('/', function (req, res) {
-    // res.sendFile();
-    console.log("hello")
+    pool.query('select now();', [], (err, result) =>{
+        done();
+        if(err){
+            console.error(`query error:${err.message}`)
+        }
+        else{
+            res.body = `Query success, data is: ${result.rows[0].now}`;
+            // console.log(`Query success, data is: ${result.rows[0].now}`);
+        }
+    });
 });
 
 var server = app.listen(process.env.PORT || 8080, function () {
---
+++ connectDB.js
@@ -0,0 +1,41 @@
+// connect via pool or client
+const { Pool, Client } = require('pg');
+const pool = new Pool({
+    user: process.env.DB_user,
+    password: process.env.DB_password,
+    host: process.env.DB_host,
+    database: process.env.DB,
+    port: process.env.DB_port,
+    ssl: true
+});
+
+function initDB() {
+    pool.connect(function(err, client, done) {
+        if(err){
+            console.error(`Connect query:${err.message}`);
+            return;
+        }
+
+        client.query('select now();', [], (err, result) =>{
+            done();
+            if(err){
+                console.error(`query error:${err.message}`)
+            }
+            else{
+                console.log(`Query success, data is: ${result.rows[0].now}`);
+            }
+        });
+    });
+}
+
+function getPool() {
+    return pool;
+}
+
+//
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// });
+
+module.exports = { initDB, getPool };
