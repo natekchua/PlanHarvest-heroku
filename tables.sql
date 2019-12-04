369fd77aa7c76804c2ff02359fb73c208341e7ca
Daniel Stamper <stamper@fastmail.com>
2019-12-04 16:05:34 -0700

    tables changes

--- tables.sql
+++ tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Customer (
-CustomerID int,
+CustomerID SERIAL,
 FirstName varchar(255) NOT NULL,
 LastName varchar(255) NOT NULL,
 Location varchar(255) NOT NULL,
@@ -8,14 +8,14 @@ PRIMARY KEY (CustomerID)
 );
 
 CREATE TABLE Farm (
-FarmID int,
+FarmID SERIAL,
 OfficeLocation varchar(255) NOT NULL,
 DateFounded DATE,
 PRIMARY KEY (FarmID)
 );
 
 CREATE TABLE Field (
-FieldID int,
+FieldID SERIAL,
 FarmID int NOT NULL,
 Location varchar(255) NOT NULL,
 FieldSize real NOT NULL,
@@ -24,41 +24,25 @@ PRIMARY KEY (FieldID),
 FOREIGN KEY (FarmID) REFERENCES Farm (FarmID)
 );
 
-/*
-CREATE TABLE Rents (
-OwnerID int NOT NULL,
-RenterID int NOT NULL,
-FieldID int NOT NULL,
-StartDate DATE DEFAULT CURRENT_DATE,
-EndDate DATE DEFAULT CURRENT_DATE + INTERVAL '1 month',
-Cost real DEFAULT 0 NOT NULL,
-PRIMARY KEY (StartDate)
-FOREIGN KEY (OwnerID) REFERENCES Farm (FarmID),
-FOREIGN KEY (RenterID) REFERENCES Farm (FarmID),
-FOREIGN KEY (FieldID) REFERENCES Field (FieldID)
-);
-*/
-
 CREATE TABLE Contracts (
-ContractID int,
+ContractID SERIAL,
 CustomerID int NOT NULL,
 FarmID int NOT NULL,
-numOfLoads int DEFAULT 0,
+numOfLoads int DEFAULT 1,
 StartDate  DATE DEFAULT CURRENT_DATE,
 DeliverByDate DATE NOT NULL,
 Delivered BIT DEFAULT '0'::"bit",
 Rejected BIT DEFAULT '0'::"bit",
-FarmOrCustAct BIT DEFAULT '0'::"bit",
 PRIMARY KEY (ContractID),
 FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID),
 FOREIGN KEY (FarmID) REFERENCES Farm (FarmID)
 );
 
 CREATE TABLE Product (
-ProductID int,
+ProductID SERIAL,
 Grade int NOT NULL,
 DateStored DATE DEFAULT CURRENT_DATE,
-FieldID int NOT NULL,
+FieldID int,
 PRIMARY KEY (ProductID),
 FOREIGN KEY (FieldID) REFERENCES Field (FieldID)
 );
@@ -71,9 +55,9 @@ FOREIGN KEY (ProductID) REFERENCES Product (ProductID)
 );
 
 CREATE TABLE Shed (
-ShedID int,
-NumRows int DEFAULT 0,
-StacksPerRow int DEFAULT 0,
+ShedID SERIAL,
+NumRows int DEFAULT 2,
+StacksPerRow int DEFAULT 10, 
 FieldID int NOT NULL,
 Location varchar(255) NOT NULL,
 PRIMARY KEY (ShedID),
@@ -81,16 +65,18 @@ FOREIGN KEY (FieldID) REFERENCES Field (FieldID)
 );
 
 CREATE TABLE Bin (
-BinID int,
-volumetricCapacity real DEFAULT 0,
+BinID SERIAL,
+VolumetricCapacity real DEFAULT 10, 
+VolumeFilled real DEFAULT 0,
 FieldID int NOT NULL,
 Location varchar(255) NOT NULL,
+ProductType varchar(255) NOT NULL,
 PRIMARY KEY (BinID),
 FOREIGN KEY (FieldID) REFERENCES Field (FieldID)
 );
 
 CREATE TABLE Bale (
-ProductID int NOT NULL,
+ProductID SERIAL,
 BaleType varchar(255) NOT NULL,
 ShedID int NOT NULL,
 FOREIGN KEY (ProductID) REFERENCES Product (ProductID) ON DELETE CASCADE,
